6ec2b529127c4abca3ea14204510d330d5c3088e
Cote Feldsine <cotefeldsine@Cotes-MacBook-Pro.local>
2021-10-26 11:57:44 -0700

    Finished problem 2

---
+++ Assignment_2/problem2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+
+void merge(int arr[], int begin, int middle, int end){
+  int i1 = begin, i2 = middle + 1;
+  int new_array[end-begin+1];
+
+  int new_array_index = 0;
+  while (i1 <= middle && i2 <= end){
+    if (arr[i1] < arr[i2]){
+      new_array[new_array_index++] = arr[i1++];
+    } else {
+      new_array[new_array_index++] = arr[i2++];
+    }
+  }
+
+  while (i1 <= middle){
+    new_array[new_array_index++] = arr[i1++];
+  }
+  while (i2 <= end){
+    new_array[new_array_index++] = arr[i2++];
+  }
+
+  i1 = begin;
+  for (new_array_index = 0; new_array_index <= end-begin; new_array_index++){
+    arr[i1++] = new_array[new_array_index];
+  }
+}
+
+void merge_sort(int arr[], int begin, int end){
+  int middle = (begin + end)/2;
+  if (begin < middle){
+    merge_sort(arr, begin, middle);
+  }
+  if (middle+1 < end){
+    merge_sort(arr, middle+1, end);
+  }
+  merge(arr, begin, middle, end);
+}
+
+void sort(int arr[], int size){
+   merge_sort(arr, 0, size-1);
+}
+
+int main() {
+  string input;
+  cin >> input;
+  cin.ignore();
+  int size = stoi(input);
+
+  int scoreVect[size];
+
+  for (int i=0;i<size;i++){
+    cin >> input;
+    cin.ignore();
+    scoreVect[i] = stoi(input);
+  }
+
+  sort(scoreVect, size);
+
+  int departments_required = (size/2) + 1;
+  int people_required = 0;
+  for (int i=0;i < departments_required; i++){
+    people_required += (scoreVect[i]/2) + 1;
+  }
+
+  cout << people_required << endl;
+  
+  return 0;
+}
