ff973e0720e9a8681a8a47085b3f7c60004a5592
Cote Feldsine <cfeldsine@gmail.com>
2021-11-24 09:09:12 -0800

    added knapsack

--- Reference/knapsack.cpp
+++ Reference/knapsack.cpp
@@ -4,25 +4,30 @@ using namespace std;
 
 int max(int a, int b) { return (a > b) ? a : b; }
 
-
-int knapSack (int maxWeight, int weights[], int values[], int size){
-  int curBest = 0;
+int knapSack (int maxWeight, int weights[], int values[], int size, int ans[]){
   // cout << "knapSack(" << maxWeight << ")" << endl;
+  // cout << "Stored value: " << ans[maxWeight] << endl;
+  if (ans[maxWeight] != -1) {
+    return ans[maxWeight];
+  }
+  int curBest = 0;
   for (int i=0;i<size;i++){
     if (maxWeight >= weights[i]){
-      curBest = max(curBest, knapSack(maxWeight-weights[i], weights, values, size-1) + values[i]);
-      // cout << curBest << endl;
+      curBest = max(curBest, knapSack(maxWeight-weights[i], weights, values, size-1, ans) + values[i]);
+      // cout << "Branch " << i << ": " << curBest << endl;
     }
   }
-  return curBest;
+  return ans[maxWeight] = curBest;
 }
 
 int main()
 {
-    int val[] = { 60, 100, 120 };
-    int wt[] = { 10, 20, 30 };
-    int W = 50;
+    int val[] = { 150, 100, 10 };
+    int wt[] = { 5, 4, 2 };
+    int W = 8;
+    int ans[W + 1];
+    fill_n (ans, W + 1, -1);
     int n = sizeof(val) / sizeof(val[0]);
-    cout << knapSack(W, wt, val, n);
+    cout << knapSack(W, wt, val, n, ans);
     return 0;
 }
